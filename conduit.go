d17b8781360f989a1d6a1b8c7e8e54ff2dd9fffd
JoeyShapiro <joeyashapiro@gmail.com>
2025-01-31 16:10:04 -0600

    basic commands

--- conduit.go
+++ conduit.go
@@ -1,14 +1,20 @@
 package main
 
-import "github.com/boltdb/bolt"
+import (
+	"encoding/binary"
+	"encoding/json"
+
+	"github.com/boltdb/bolt"
+)
+
+type Statement struct {
+	store  *Conduit
+	bucket string
+	where  func(entry any) bool
+}
 
 type Conduit struct {
 	db *bolt.DB
-}
-
-type Statement struct {
-	bucket string
-	where  func(entry any) bool
 }
 
 type View struct {
@@ -24,8 +30,19 @@ func New(file string) (store Conduit, err error) {
 	return store, err
 }
 
-func (store *Conduit) From(bucket string) (s *Statement) {
-	s.bucket = bucket
+func (store *Conduit) Create(bucket string) error {
+	err := store.db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte(bucket))
+		return err
+	})
+	return err
+}
+
+func (store *Conduit) From(bucket string) *Statement {
+	s := &Statement{
+		store:  store,
+		bucket: bucket,
+	}
 
 	return s
 }
@@ -35,6 +52,82 @@ func (s *Statement) Where(where func(entry any) bool) *Statement {
 	return s
 }
 
+func (s *Statement) Insert(object any) error {
+	content, err := json.Marshal(object)
+	if err != nil {
+		return err
+	}
+
+	err = s.store.db.Update(func(tx *bolt.Tx) (err error) {
+		bucket := tx.Bucket([]byte(s.bucket))
+
+		primaryKey, err := bucket.NextSequence()
+		if err != nil {
+			return err
+		}
+
+		// convert pk to unsigned int byte array
+		bePK := make([]byte, 8)
+		binary.BigEndian.PutUint64(bePK, uint64(primaryKey))
+
+		// store in the bucket and return the error, if any
+		return bucket.Put(bePK, content)
+	})
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (s *Statement) Update(pk uint64, object any) (err error) {
+	content, err := json.Marshal(object)
+	if err != nil {
+		return err
+	}
+
+	err = s.store.db.Update(func(tx *bolt.Tx) (err error) {
+		bucket := tx.Bucket([]byte(s.bucket))
+
+		// convert pk to unsigned int byte array
+		bePK := make([]byte, 8)
+		binary.BigEndian.PutUint64(bePK, uint64(pk))
+
+		// store in the bucket and return the error, if any
+		return bucket.Put(bePK, content)
+	})
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
 func (s *Statement) Execute() (results []any, err error) {
+	// create the view for the db
+	err = s.store.db.View(func(tx *bolt.Tx) error {
+		// open the bucket using its name
+		bucket := tx.Bucket([]byte(s.bucket))
+
+		// for every row
+		bucket.ForEach(func(k, v []byte) error {
+			// convert the byte array into an entry
+			var entry any
+			err := json.Unmarshal(v, &entry)
+			if err != nil {
+				return err
+			}
+
+			// if the entry matches the where condition specified by the user
+			if s.where(entry) {
+				// append to the results
+				results = append(results, entry)
+			}
+
+			return nil
+		})
+		return nil
+	})
+
 	return
 }
--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
 	fmt.Println("hello world")
 
@@ -12,5 +17,23 @@ func main() {
 		fmt.Println(err)
 	}
 
-	conduit.From("benchmarks").Execute()
+	err = conduit.Create("users")
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		Username: "root",
+		Password: "toor",
+	}
+	err = conduit.From("users").Insert(user)
+	if err != nil {
+		panic(err)
+	}
+
+	results, err := conduit.From("users").Where(func(entry any) bool { return true }).Execute()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(results)
 }
